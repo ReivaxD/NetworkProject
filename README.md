7b943f4216bdf3af608f7e452159ecd3f412e1da
ReivaxD <xavier.argentines@hotmail.com>
2025-09-22 09:27:16 +0200

    Add project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# NetworkProject
+# Projet-Reseau
+
+Pour lancer ce programme, lancer le fichier simulateur/reso/examples/dv_routing/Demo.java
+(ne pas oublier de compiler avant de lancer si modifications il y a eu)
+
+-> main de Demo.java est la méthode principale :
+
+  -> String filename= "reso/examples/dv_routing/demo-graph.txt" permet de choisir le modèle à instancier
+  Pour revenir au fichier de base (demo-graph.txt étant l'exercice 1) remplacer reso/examples/dv_routing/demo-graph.txt par reso/data/topology.txt
+  
+  -> scheduler.run() lance l'algo avec le router A comme récepteur du 1er signal (via setupRoutingProtocol(network, "A") )
+  -> scheduler.runUntil(0.100) lance l'algo avec un timing max avant arret (en cas de récursion infinie)
+  
+Exercice 2 presque fini, tout est la en commentaire dans main, seulement, il reste à trouver le changement d'arc provoquant une boucle infinie
+Note : boucle infinie n'est pas vraiment infinie, c'est le concept vu en cours
---
+++ simulateur/reso/examples/dv_routing/Infinity.java
@@ -0,0 +1,93 @@
+package reso.examples.dv_routing;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import reso.common.Network;
+import reso.common.Node;
+import reso.ip.IPAddress;
+import reso.ip.IPHost;
+import reso.ip.IPInterfaceAdapter;
+import reso.ip.IPLayer;
+import reso.ip.IPRouter;
+import reso.scheduler.AbstractScheduler;
+import reso.scheduler.Scheduler;
+import reso.utilities.FIBDumper;
+import reso.utilities.NetworkBuilder;
+import reso.utilities.NetworkGrapher;
+
+import reso.examples.dv_routing.DVRoutingProtocol;
+
+/**
+ * Infinity
+ */
+public class Infinity {	
+	private static IPAddress getRouterID(IPLayer ip) {
+		IPAddress routerID= null;
+		for (IPInterfaceAdapter iface: ip.getInterfaces()) {
+			IPAddress addr= iface.getAddress();
+			if (routerID == null)
+				routerID= addr;
+			else if (routerID.compareTo(addr) < 0)
+				routerID= addr;
+		}
+		return routerID;
+	}
+	
+	/**
+	 * Configure the routing protocol on every router.
+	 * If routerDst is not null, configure a single router as destination.
+	 * 
+	 * @param network
+	 * @param routerDst
+	 * @throws Exception
+	 */
+	private static void setupRoutingProtocol(Network network, String routerDst)
+		throws Exception
+	{
+		for (Node n: network.getNodes()) {
+			if (!(n instanceof IPRouter))
+				continue;
+			IPRouter router= (IPRouter) n;
+			boolean advertise= (routerDst == null) || (n.name.equals(routerDst));
+			router.addApplication(new DVRoutingProtocol(router, advertise));
+			router.start();
+		}
+	}
+	
+	public static void main(String [] args) {
+		try {
+			String filename= "reso/examples/dv_routing/demo-graph.txt";
+            AbstractScheduler scheduler= new Scheduler();
+            Network network= NetworkBuilder.loadTopology(filename, scheduler);
+            setupRoutingProtocol(network, "A");
+                
+            // Run simulation -- first convergence
+			scheduler.run();
+
+			((IPHost) network.getNodeByName("B")).getIPLayer().getInterfaceByName("eth0").setMetric(60);
+			((IPHost) network.getNodeByName("B")).getIPLayer().getInterfaceByName("eth1").setMetric(60);
+
+			//DVrouting attrchanged a modifier
+				//appeller a nouveau methode qui va recalculer les routes
+
+			// lien longueur en metre et vitesse de transmission en bit/sec (2/3 vitesse lumiere)
+
+            // Display forwarding table for each node
+            FIBDumper.dumpForAllRouters(network);			
+                            
+            // Run simulation (for 0.1 sec (no))
+            scheduler.runUntil(5);
+                            
+            // Display forwarding table for each node
+            FIBDumper.dumpForAllRouters(network);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
